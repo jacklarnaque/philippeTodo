ace850d5ea4b01809c1ab96d64e315cae1bd2751
jacklarnaque <grousset1065@live.fr>
2023-01-25 21:52:47 +0000

    final style before touching the algo of the app

--- src/components/SideTasks.jsx
+++ src/components/SideTasks.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SideTasks() {
   return (
     <div className='sideTasks'>
-        <h2>Side Tasks</h2>
+        <h2>Side Tasks:</h2>
         </div>
   )
 }
